aba05af2edf3254a0af379965668df7e263407f8
KHS <kij4300@naver.com>
2020-02-11 08:46:48 +0900

    Create TOC.js

---
+++ Egoing/4/components/TOC.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+// TOC: 테이블 오브 컨텐츠. 목차.
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC;
