eeac4f170d42ca11f3b6f2fcb12a7d30ba45f8bc
KHS <kij4300@naver.com>
2020-02-11 13:39:33 +0900

    Create App.js

---
+++ etc/InputOutputEvent/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import EventPractice from "./components/EventPractice";
+import "./App.css";
+
+const App = () => {
+  return <EventPractice />;
+};
+
+export default App;
