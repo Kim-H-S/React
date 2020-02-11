7ba5ab1670f01284655d859db50b6ed69a2f4475
KHS <kij4300@naver.com>
2020-02-11 15:40:23 +0900

    Create App.js

---
+++ etc/WordAdd/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import IterationSample from "./components/IterationSample";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <IterationSample />
+    </div>
+  );
+};
+
+export default App;
