a653af06f046f6a457ae4a25a4ef59740f7c2e24
KHS <kij4300@naver.com>
2020-02-13 08:17:30 +0900

    Create Contens.js

---
+++ etc/SectionBreak/components/Contens.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Contens extends Component {
+  render() {
+    return (
+      <div id="contents">
+        <h1>TEST</h1>
+      </div>
+    );
+  }
+}
+
+export default Contens;
