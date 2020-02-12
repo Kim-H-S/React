8dc671c3d11b32ed4aa3afe2953a88846036aaad
KHS <kij4300@naver.com>
2020-02-13 08:16:42 +0900

    Create App.js

---
+++ etc/SectionBreak/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Contens from "./components/Contens";
+import Menu from "./components/Menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Contens></Contens>
+      <Menu></Menu>
+    </div>
+  );
+}
+
+export default App;
